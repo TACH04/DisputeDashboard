19574d7ea23fc35e0a2efe8188a440408d72ae99
Tanner Hochberg <tanner.hochberg@gmail.com>
2025-06-13 21:29:20 -0700

    Fixed preload.js

--- preload.js
+++ preload.js
@@ -7,5 +7,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   askGemini: (prompt) => ipcRenderer.invoke('ask-gemini', prompt),
   generateLetter: (disputes) => ipcRenderer.invoke('generate-letter', disputes),
-  uploadAndProcess: (requests) => ipcRenderer.invoke('upload-and-process', requests) // <-- ADD THIS LINE
+  uploadAndProcess: (requests) => ipcRenderer.invoke('upload-and-process', requests)
 });
